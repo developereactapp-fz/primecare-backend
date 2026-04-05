298d7a3147da9d4266386cc49830fc08d2a7897f
PrimeCare Developer <develope.reactapp@gmail.com>
2026-04-05 13:08:47 +0530

    Initial backend setup

--- config/mailer.js
+++ config/mailer.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp.titan.email",
   port: 587,
   secure: false,
   auth: {
