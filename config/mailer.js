4c4f4aa4732ed9e2f2c06cb4b954274702649eeb
PrimeCare Developer <develope.reactapp@gmail.com>
2026-04-05 13:02:20 +0530

    Initial backend setup

--- config/mailer.js
+++ config/mailer.js
@@ -1,9 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
-  secure: false, // true only for port 465
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
